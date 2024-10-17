c4849f51c1b5197c29deb353d6569063422da42d
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-10-17 10:41:39 +0530

    Create IsPrime.py

---
+++ IsPrime.py
@@ -0,0 +1,10 @@
+n = int(input())
+is_prime = True
+for i in range(2,n):
+   if n%i==0:
+       is_prime=False
+       break
+if is_prime==True:
+      print("Prime")
+else:
+      print("Not Prime")
