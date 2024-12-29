d49ab77e8405be1513ee4e62fad78131645daf07
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-12-29 18:49:11 +0530

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,10 @@
+def is_palindrome(s):
+    s = s.lower()
+    return s == s[::-1]
+
+string = input("Enter a string: ")
+
+if is_palindrome(string):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
