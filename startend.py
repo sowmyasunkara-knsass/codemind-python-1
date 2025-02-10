fd98311f9a6ee57c80343f88848fdd2be14e66b9
babysowmya <147791943+babysowmya@users.noreply.github.com>
2025-02-10 18:57:09 +0530

    Create startend.py

---
+++ startend.py
@@ -0,0 +1,12 @@
+my_list = [1,2,3,4,5]
+new_element = 6
+my_list.append(new_element)
+print(my_list)
+element_to_insert = 7
+pos = 2
+my_list.insert(pos,element_to_insert)
+print(my_list)
+start = 1
+end = 2
+sliced = my_list[start:end]
+print(sliced)
