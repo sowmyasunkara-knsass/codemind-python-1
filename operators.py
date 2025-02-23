0d8b943e57e3b2e49663bfafb38d42622a7a3c8b
babysowmya <147791943+babysowmya@users.noreply.github.com>
2025-02-23 14:25:13 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,41 @@
+print("Arthimetic operators:")
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b) 
+print("Relation operators:")
+x = 10
+y = 20
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+print(x>=y)
+print(x<=y)
+print("assignment operators:")
+a=5
+a+=6
+print(a)
+a*=5
+print(a)
+print("LogiCal opreators:")
+c = True
+d = False
+print(c and d)
+print(c or d)
+print(not c)
+print("Bitwise operators:")
+a=5
+b=3
+print(a&b)
+print(a|b)
+print(a^b)
+print("Terinary operators:")
+x = 10
+y = 20
+res = "x is greater" if x>y else "y is greater"
+print(res)
