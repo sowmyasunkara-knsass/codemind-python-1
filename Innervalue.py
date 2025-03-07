2549c42047ba345826e0a3a72af3fdd804fca10d
babysowmya <147791943+babysowmya@users.noreply.github.com>
2025-03-07 19:26:46 +0530

    Create Innervalue.py

---
+++ Innervalue.py
@@ -0,0 +1,6 @@
+r,c = 3,2
+mat = [[10,20],[30,40],[50,60]]
+for each_inner_list in mat: #meach_inner_list = [10,20]
+    for each_value in each_inner_list: 
+        print(each_value,end = ' ')
+    print()
