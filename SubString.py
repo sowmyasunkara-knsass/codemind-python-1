67a5aabdcbeba1f1ba7f7c7bda35fcde0c7b8bd8
babysowmya <147791943+babysowmya@users.noreply.github.com>
2025-04-08 20:57:03 +0530

    Create SubString.py

---
+++ SubString.py
@@ -0,0 +1,6 @@
+main_string = input()
+sub_string = input()
+if sub_string in main_string:
+    print("SubString present in the main String")
+else:
+    print("SubString not present in the main String")
